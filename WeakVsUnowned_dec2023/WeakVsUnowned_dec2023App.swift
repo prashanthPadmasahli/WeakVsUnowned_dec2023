b67625a77f0a1bdb9ecad2aa825e46b81d3cd85d
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2023-12-26 21:13:36 +0530

    Initial Commit

---
+++ WeakVsUnowned_dec2023/WeakVsUnowned_dec2023App.swift
@@ -0,0 +1,17 @@
+//
+//  WeakVsUnowned_dec2023App.swift
+//  WeakVsUnowned_dec2023
+//
+//  Created by mac on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeakVsUnowned_dec2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
